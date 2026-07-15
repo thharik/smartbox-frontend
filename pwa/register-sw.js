511ca5ea4fe9b2a03227a3318ee924db146d2a74
tharikluan <tharikluan.miuel@rede.ulbra.br>
2026-07-15 20:35:21 -0300

    Aplica scope / no registro do service worker

--- pwa/register-sw.js
+++ pwa/register-sw.js
@@ -5,7 +5,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/pwa/service-worker.js")
+      .register("/pwa/service-worker.js", { scope: "/" })
       .then((reg) => {
         console.log("SW registrado:", reg.scope);
 
